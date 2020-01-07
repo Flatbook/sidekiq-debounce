889000a1d0fa61e9b110ad6935720fbe08222bea
Alex Girdler <alex.girdler@sonder.com>
2020-01-07 13:01:12 -0800

    Spec out debounce_by

--- spec/sidekiq/debounce_spec.rb
+++ spec/sidekiq/debounce_spec.rb
@@ -10,6 +10,14 @@ class DebouncedWorker
   def perform(_a, _b); end
 end
 
+class  DebouncedByWorker
+  include Sidekiq::Worker
+  sidekiq_options debounce: true,
+                  debounce_by: -> (args) { args[0...-1] }
+
+  def perform(_a, _b, _ignored); end
+end
+
 describe Sidekiq::Debounce do
   before do
     stub_scheduled_set
@@ -20,30 +28,62 @@ describe Sidekiq::Debounce do
   end
 
   let(:set) { Sidekiq::ScheduledSet.new }
-  let(:sorted_entry) { Sidekiq::SortedEntry.new(set, 0, {jid: '54321'}.to_json) }
 
-  it 'queues a job normally at first' do
-    DebouncedWorker.perform_in(60, 'foo', 'bar')
-    set.size.must_equal 1, 'set.size must be 1'
+  describe 'normal debouncing' do
+    let(:sorted_entry) { Sidekiq::SortedEntry.new(set, 0, {jid: '54321'}.to_json) }
+
+    it 'queues a job normally at first' do
+      DebouncedWorker.perform_in(60, 'foo', 'bar')
+      _(set.size).must_equal 1, 'set.size must be 1'
+    end
+
+    it 'ignores repeat jobs within the debounce time and reschedules' do
+      sorted_entry.expects(:reschedule)
+
+      DebouncedWorker.perform_in(60, 'foo', 'bar')
+      DebouncedWorker.perform_in(60, 'foo', 'bar')
+      _(set.size).must_equal 1, 'set.size must be 1'
+    end
+
+    it 'debounces jobs based on their arguments' do
+      DebouncedWorker.perform_in(60, 'boo', 'far')
+      DebouncedWorker.perform_in(60, 'foo', 'bar')
+      _(set.size).must_equal 2, 'set.size must be 2'
+    end
+
+    it 'creates the job immediately when given an instant job' do
+      DebouncedWorker.perform_async('foo', 'bar')
+      _(set.size).must_equal 0, 'set.size must be 0'
+    end
   end
 
-  it 'ignores repeat jobs within the debounce time and reschedules' do
-    sorted_entry.expects(:reschedule)
+  describe 'when debounce_by is set to ignore last argument' do
+    let(:sorted_entry) { Sidekiq::SortedEntry.new(set, 0, {jid: '54321'}.to_json) }
 
-    DebouncedWorker.perform_in(60, 'foo', 'bar')
-    DebouncedWorker.perform_in(60, 'foo', 'bar')
-    set.size.must_equal 1, 'set.size must be 1'
-  end
+    it 'queues a job normally at first' do
+      DebouncedByWorker.perform_in(60, 'foo', 'bar', 1)
+      _(set.size).must_equal 1, 'set.size must be 1'
+    end
 
-  it 'debounces jobs based on their arguments' do
-    DebouncedWorker.perform_in(60, 'boo', 'far')
-    DebouncedWorker.perform_in(60, 'foo', 'bar')
-    set.size.must_equal 2, 'set.size must be 2'
-  end
+    it 'ignores repeat jobs within the debounce time and reschedules' do
+      sorted_entry.expects(:reschedule)
 
-  it 'creates the job immediately when given an instant job' do
-    DebouncedWorker.perform_async('foo', 'bar')
-    set.size.must_equal 0, 'set.size must be 0'
+      DebouncedByWorker.perform_in(60, 'foo', 'bar', 2)
+      DebouncedByWorker.perform_in(60, 'foo', 'bar', 3)
+      _(set.size).must_equal 1, 'set.size must be 1'
+    end
+
+    it 'debounces jobs based on their arguments' do
+      DebouncedByWorker.perform_in(60, 'boo', 'far', 4)
+      DebouncedByWorker.perform_in(60, 'foo', 'bar', 5)
+      _(set.size).must_equal 2, 'set.size must be 2'
+    end
+
+    it 'creates the job immediately when given an instant job' do
+      DebouncedByWorker.perform_async('foo', 'bar', 6)
+      _(set.size).must_equal 0, 'set.size must be 0'
+    end
+
   end
 
   def stub_scheduled_set
